541677937b1886b1c68e36a4cc0bd520ad4e74b3
Regio-Montano <edk.tae78@gmail.com>
2026-02-19 12:50:16 -0600

    Update ModernLeadForm.js

--- src/components/ModernLeadForm.js
+++ src/components/ModernLeadForm.js
@@ -26,7 +26,7 @@ export default function ModernLeadForm() {
 
     try {
 
-      const res = await fetch("/api/submit", {
+      const res = await fetch("/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
